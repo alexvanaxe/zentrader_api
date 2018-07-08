e5874569fa4d2bf708e7818fb19863f34a119c36
Alexvanaxe <alexvanaxe@gmail.com>
2018-07-08 13:56:42 -0300

    Removing trash

--- formulas/calculator.py
+++
@@ -1,6 +0,0 @@
-"""
-Calculations of the system using the formulas
-
-To be adherent to the DRY principle, all the mathematical formulas are to be concentrated in this module, as the methods that performs the calculations itself.
-"""
-
