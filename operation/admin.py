631b44b786675fe5d173fe91d5b3d8b897f0c49a
Alexvanaxe <alexvanaxe@gmail.com>
2018-12-16 15:29:02 -0200

    Admin updated

--- operation/admin.py
+++ operation/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
-from operation.models import Operation
+from operation.models import Operation, ExperienceData, BuyData, SellData
 
 admin.site.register(Operation)
+admin.site.register(ExperienceData)
+admin.site.register(BuyData)
+admin.site.register(SellData)
 # Register your models here.
