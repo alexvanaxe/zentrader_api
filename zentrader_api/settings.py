d50b04e02b23d51c2b6529feba76800799d051d2
Alexvanaxe <alexvanaxe@gmail.com>
2019-01-06 10:29:02 -0200

    All libs updated with its respective configs

--- zentrader_api/settings.py
+++ zentrader_api/settings.py
@@ -144,7 +144,7 @@ USE_TZ = True
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
