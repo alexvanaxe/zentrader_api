80e8d608907cd685a50d0ce301d7f28c98612dad
noname <teste123@tgefdsa.com>
2019-03-15 17:15:56 -0300

    Done.

--- zen_fortune/views.py
+++ zen_fortune/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import permissions
 
 from zen_fortune.serializers import ZenFortuneSerializer
 from zen_fortune.models import Fortune
@@ -10,6 +11,8 @@ class ZenFortuneAPIView(APIView):
     Expose the fortune service via a api call. Only get is implemented to return
     the cookie.
     """
+    permission_classes = (permissions.AllowAny,)
+
     def get(self, request, format=None):
         fortune = Fortune()
 
