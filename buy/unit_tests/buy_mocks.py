0300fddc3704eb72aed271ac348bd28bec799787
John Smith <John.Smith@example.com>
2019-06-14 17:13:20 -0300

    Fixed problem

--- buy/unit_tests/buy_mocks.py
+++ buy/unit_tests/buy_mocks.py
@@ -6,6 +6,7 @@ from account.models import Account
 
 from experience.unit_tests.experience_mocks import create_experiences
 
+
 def create_operations(cls, stock, user):
     create_experiences(cls, stock, user)
 
@@ -19,29 +20,11 @@ def create_operations(cls, stock, user):
                                                            execution_date=datetime.strptime('2017-06-13T15:52:30', '%Y-%m-%dT%H:%M:%S'),
                                                            amount=150, price=18, executed=True)
 
-    cls.sell1 = SellData.objects.create(stock=stock, owner=user, account=Account.objects.all()[0],
-                                                           buy=cls.buy2,
-                                                           creation_date=datetime.strptime('2017-06-18T15:52:30', '%Y-%m-%dT%H:%M:%S'),
-                                                           execution_date=datetime.strptime('2017-06-18T15:52:30', '%Y-%m-%dT%H:%M:%S'),
-                                                           amount=50, price=22, executed=True, stop_loss=17)
-
     cls.buy3 = BuyData.objects.create(stock=stock, owner=user, account=Account.objects.all()[0],
                                                            creation_date=datetime.strptime('2017-06-25T15:52:30', '%Y-%m-%dT%H:%M:%S'),
                                                            execution_date=datetime.strptime('2017-06-25T15:52:30', '%Y-%m-%dT%H:%M:%S'),
                                                            amount=100, price=20, executed=True)
 
-    cls.sell2 = SellData.objects.create(stock=stock, owner=user, account=Account.objects.all()[0],
-                                                           buy=cls.buy2,
-                                                           creation_date=datetime.strptime('2017-06-30T15:52:30', '%Y-%m-%dT%H:%M:%S'),
-                                                           execution_date=datetime.strptime('2017-06-30T15:52:30', '%Y-%m-%dT%H:%M:%S'),
-                                                           amount=100, price=23, stop_loss=21.50, executed=True)
-
-    cls.sell3 = SellData.objects.create(stock=stock, owner=user, account=Account.objects.all()[0],
-                                        buy=cls.buy3,
-                                        creation_date=datetime.strptime('2017-06-30T15:52:30', '%Y-%m-%dT%H:%M:%S'),
-                                        execution_date=datetime.strptime('2017-06-30T15:52:30', '%Y-%m-%dT%H:%M:%S'),
-                                        amount=100,
-                                        price=23, stop_loss=13)
 
 def create_only_buy(cls, stock, user):
     cls.buy1 = BuyData.objects.create(stock=stock, owner=user, account=Account.objects.all()[0],
