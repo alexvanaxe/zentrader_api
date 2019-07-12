de3509cf09f288d1d819e8561baa5ba259cb661b
John Smith <John.Smith@example.com>
2019-07-12 17:07:19 -0300

    Pagination working

--- buy/views.py
+++ buy/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
-from rest_framework.pagination import PageNumberPagination
+from rest_framework.pagination import PageNumberPagination, Response
 from django_filters.rest_framework import DjangoFilterBackend
+from collections import OrderedDict
 
 from buy.models import BuyData
 from buy.serializers import BuyDataSerializer
@@ -13,6 +14,16 @@ class StandardResultsSetPagination(PageNumberPagination):
     page_size = 2
     page_size_query_param = 'page_size'
     max_page_size = 2
+
+    def get_paginated_response(self, data):
+        return Response(OrderedDict([
+             ('lastPage', self.page.paginator.count),
+             ('countItemsOnPage', self.page_size),
+             ('current', self.page.number),
+             ('next', self.get_next_link()),
+             ('previous', self.get_previous_link()),
+             ('results', data)
+         ]))
 
 
 class BuyDataViewSet(viewsets.ModelViewSet):
