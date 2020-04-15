b353308cc573967e1f0116082c88afed4fa4639b
Alexvanaxe <alexvanaxe@gmail.com>
2020-04-15 20:11:13 -0300

    Fixing wrong field

--- stock/views.py
+++ stock/views.py
@@ -23,7 +23,7 @@ class StockViewSet(viewsets.ModelViewSet):
     """
     A viewset representing the stock.
     """
-    queryset = get_stock_queryset().order_by('operations_t_null')
+    queryset = get_stock_queryset().order_by('operations_t')
     serializer_class = StockSerializer
 
 
