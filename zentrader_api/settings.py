31f6ca3f487b440438c572b3d6122d7674d436c5
AVA <alexvanaxe@gmail.com>
2023-01-12 20:13:50 -0300

    Updating the database information

--- zentrader_api/settings.py
+++ zentrader_api/settings.py
@@ -105,7 +105,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'zentrader',
-        'USER': '',
+        'USER': 'zentrader',
         'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '5432',
